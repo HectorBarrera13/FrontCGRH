e940b52f1ea8cce87b1fdee0bbcf44e30ec63957
HectorBarrera13 <hectoralejandro.bc@gmail.com>
2026-01-15 14:38:41 -0600

    Basic functions login and payroll table

--- my-front/src/components/Filter.tsx
+++ my-front/src/components/Filter.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useId, useRef, useState } from "react";
+import { useEffect, useId, useRef, useState } from "react";
 import { ChevronsUpDown } from "./../assets/icons";
 
 type FilterOption = {
